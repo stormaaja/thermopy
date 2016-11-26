9dde45675ee951dd87ea8914392dbb703abeff0f
Matti Ahinko <matti.ahinko@gmail.com>
2016-11-26 20:33:52 +0200

    Add non tested and non working logger

---
+++ thermo_logger.py
@@ -0,0 +1,75 @@
+#!/bin/python
+import time
+import datetime
+import signal
+import sys
+import os
+import glob
+import RPi.GPIO as GPIO
+
+TARGET_TEMPERATURE_CHANGE = 5
+LOG_FILENAME = "{0}.csv".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+START_TIME = time.time()
+BASE_DIR = '/sys/bus/w1/devices/'
+DEVICE_FOLDER = glob.glob(BASE_DIR + '28*')[0]
+DEVICE_FILE = DEVICE_FOLDER + '/w1_slave'
+RELAY_PIN = 4
+
+def get_running_time():
+    return time.time() - START_TIME
+
+def read_temperature():
+    return
+
+def set_heating(enable):
+    not_used_yet = enable
+    return enable
+
+def log_temperature(running_time, temperature, heating):
+    f = open(LOG_FILENAME, 'a')
+    f.write("{0},{1},{2}\n".format(running_time, temperature, heating))
+    f.close()
+
+def read_temperature_raw_lines():
+    f = open(DEVICE_FILE, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temperature_raw_lines()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temperature_raw_lines()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
+def signal_handler(signal, frame):
+    GPIO.cleanup()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+heating = set_heating(True)
+target = read_temperature() + TARGET_TEMPERATURE_CHANGE
+
+while True:
+    current_temperature = read_temperature()
+    current_running_time = get_running_time()
+
+    log_temperature(get_running_time(), current_temperature, heating)
+    print("\r{0:.2f}: {1:.2f} {2}"\
+        .format(current_running_time, current_temperature, heating), end="")
+
+    if heating and current_temperature >= TARGET_TEMPERATURE:
+        heating = set_heating(False)
+    time.sleep(1)
