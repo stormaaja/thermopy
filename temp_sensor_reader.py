6e218e8d16539a59f2e1fdb1a2a922b3c8954e28
Matti Ahinko <matti.ahinko@gmail.com>
2016-12-15 22:36:39 +0200

    Refactor and clean

--- temp_sensor_reader.py
+++ temp_sensor_reader.py
@@ -1,27 +1,27 @@
+from temperature_not_valid import TemperatureNotValid
 
 class TempSensorReader:
 
     def __init__(self, path: str):
         self.path = path
 
-    def get_temperature(self) -> float:
-        return 0.0
+    def read_temperature(self) -> float:
+        lines = read_temperature_raw_lines()
+        parse_temperature_from_lines(lines)
 
-    def read_temperature_raw_lines(self) -> list:
+    def read_raw_lines(self) -> list:
         f = open(self.path, 'r')
         lines = f.readlines()
         f.close()
         return lines
 
-    def read_temp_from_lines(self, lines: list) -> float:
-        lines = read_temperature_raw_lines()
-        while lines[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = read_temperature_raw_lines()
-        equals_pos = lines[1].find('t=')
+    def parse_temperature_from_lines(self, lines: list) -> float:
+        if len(lines) < 2 or 'YES' not in lines[0] or 't=' not in lines[1]:
+            raise TemperatureNotValid(
+                "Temperature is not valid or it is not available")
 
-        if equals_pos == -1: raise RuntimeError("No data available")
+        temperature_position = lines[1].find('t=')
 
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
+        temperature_string = lines[1][temperature_position + 2:]
+        temperature = float(temperature_string) / 1000.0
         return temp_c
