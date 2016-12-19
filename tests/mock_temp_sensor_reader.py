a3d1b04223d5b0dfc7f478b61a86f21c01489687
Matti Ahinko <matti.ahinko@gmail.com>
2016-12-19 22:09:33 +0200

    Set default temperature

--- tests/mock_temp_sensor_reader.py
+++ tests/mock_temp_sensor_reader.py
@@ -4,6 +4,7 @@ class MockTempSensorReader(TempSensorReader):
 
     def __init__(self, path: str):
         self.path = path
+        self.set_temperature(0.0)
 
     def read_temperature(self) -> float:
         return self.temperature
