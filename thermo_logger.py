3e454adb6440e224f43cde855cf795173b67dc2c
Matti Ahinko <matti.ahinko@gmail.com>
2016-12-19 20:32:03 +0200

    Move thermo logger to own class

--- thermo_logger.py
+++ thermo_logger.py
@@ -11,49 +11,73 @@ from temp_sensor_reader import TempSensorReader
 
 TARGET_TEMPERATURE_CHANGE = 5
 LOG_FILENAME = "{0}.csv".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
-START_TIME = time.time()
 BASE_DIR = '/sys/bus/w1/devices/'
 DEVICE_FOLDER = glob.glob(BASE_DIR + '28*')[0]
 DEVICE_FILE = DEVICE_FOLDER + '/w1_slave'
 RELAY_PIN = 4
 
-temp_sensor_reader = TempSensorReader(DEVICE_FILE)
+class ThermoLogger:
 
-def get_running_time():
-    return time.time() - START_TIME
+    def __init__(self, log_file: str):
+        self.start_time = time.time()
+        self.log_file = log_file
 
-def set_heating(enable):
-    not_used_yet = enable
-    return enable
+    def set_temp_sensor_reader(self, device_file: str):
+        self.temp_sensor_reader = TempSensorReader(device_file)
 
-def log_temperature(running_time, temperature, heating):
-    f = open(LOG_FILENAME, 'a')
-    f.write("{0},{1},{2}\n".format(running_time, temperature, heating))
-    f.close()
+    def read_temperature(self) -> float:
+        return self.temp_sernor_reader.read_temperature()
 
-def signal_handler(signal, frame):
-    GPIO.cleanup()
-    sys.exit(0)
+    def set_heating_relay(self, pin: int):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
 
-signal.signal(signal.SIGINT, signal_handler)
+    def clean(self):
+        GPIO.cleanup()
+
+    def get_running_time(self):
+        return self.time.time() - self.start_time
+
+    def set_heating(self, heating: bool):
+        self.heating = heating
+        return
+
+    def log_temperature(self, running_time, temperature, heating):
+        f = open(self.log_file, 'a')
+        f.write("{0},{1},{2}\n".format(running_time, temperature, heating))
+        f.close()
+
+    def set_target_temperature(self, target_temperature: int):
+        self.target_temperature = target_temperature
+
+    def run(self):
+        self.set_heating(True)
+
+        while True:
+            current_temperature = self.read_temperature()
+            current_running_time = self.get_running_time()
+
+            log_temperature(get_running_time(), current_temperature, heating)
+
+            print("\r{0:.2f}: {1:.2f} {2}"\
+                .format(current_running_time, current_temperature, heating), end="")
+
+            if self.heating and current_temperature >= self.target_temperature:
+                set_heating(False)
+            time.sleep(1)
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RELAY_PIN, GPIO.OUT)
+thermo_logger = ThermoLogger(LOG_FILENAME)
 
-heating = set_heating(True)
-target = temp_sensor_reader.read_temperature() + TARGET_TEMPERATURE_CHANGE
+def signal_handler(signal, frame):
+    thermo_logger.clean()
+    sys.exit(0)
 
-while True:
-    current_temperature = read_temperature()
-    current_running_time = get_running_time()
+signal.signal(signal.SIGINT, signal_handler)
 
-    log_temperature(get_running_time(), current_temperature, heating)
-    print("\r{0:.2f}: {1:.2f} {2}"\
-        .format(current_running_time, current_temperature, heating), end="")
-
-    if heating and current_temperature >= TARGET_TEMPERATURE:
-        heating = set_heating(False)
-    time.sleep(1)
+thermo_logger.set_temp_sensor_reader(DEVICE_FILE)
+thermo_logger.set_target_temperature(
+    thermo_logger.
+thermo_logger.run()
