5ad8333094532deabb566023d6a95dc606dba3a9
Matti Ahinko <matti.ahinko@gmail.com>
2016-12-19 20:52:26 +0200

    Add setting state

--- heating_relay.py
+++ heating_relay.py
@@ -5,6 +5,10 @@ class HeatingRelay:
     def __init__(self, pin: int):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.OUT)
+        self.pin = pin
 
     def clean(self):
         GPIO.cleanup()
+
+    def set_heating(self, heating: bool):
+        GPIO.output(self.pin, heating)
