6b6231ad4d8d08963000c170accc6d28c2d8a9de
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-23 21:42:01 +0200

    Use heating relay instead

--- thermo_logger.py
+++ thermo_logger.py
@@ -3,7 +3,7 @@ import time
 
 from csv_logger import CSVLogger
 from thermostat import Thermostat
-from relay import Relay
+from heating_relay import HeatingRelay
 
 class ThermoLogger:
 
@@ -16,19 +16,19 @@ class ThermoLogger:
     def set_thermostat(self, thermostat: Thermostat):
         self.thermostat = thermostat
 
-    def set_heating_relay(self, heating_relay: Relay):
+    def set_heating_relay(self, heating_relay: HeatingRelay):
         self.heating_relay = heating_relay
 
     def get_running_time(self):
         return time.time() - self.start_time
 
     def run(self):
-        self.heating_relay.set_state(True)
+        self.heating_relay.set_heating(True)
 
         while True:
             current_temperature = self.thermostat.read_current_temperature()
             current_running_time = self.get_running_time()
-            heating = self.heating_relay.get_state()
+            heating = self.heating_relay.is_heating()
 
             self.csv_logger.log(
                 self.get_running_time(), current_temperature, heating)
@@ -38,6 +38,6 @@ class ThermoLogger:
                 end="")
 
             if heating and not self.thermostat.should_heat():
-                self.heating_relay.set_state(False)
+                self.heating_relay.set_heating(False)
 
             time.sleep(1)
