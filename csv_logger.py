8b835af1e0357fddd593cd03bf27714f4d883dfc
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-23 21:41:26 +0200

    Use right member

--- csv_logger.py
+++ csv_logger.py
@@ -6,6 +6,6 @@ class CSVLogger:
         self.file = file
 
     def log(self, running_time, temperature, heating):
-        f = open(self.log_file, 'a')
+        f = open(self.file, 'a')
         f.write("{0},{1},{2}\n".format(running_time, temperature, heating))
         f.close()
